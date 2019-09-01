9385277c38e1b0daba1ce7e1eb64167cc0f5aaed
Mark Jeronimus <mark.jeronimus@gmail.com>
2019-09-01 12:58:58 +0200

    Fix Copyleft year

--- src/org/digitalmodular/utilities/container/Tuple.java
+++ src/org/digitalmodular/utilities/container/Tuple.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of AllUtilities.
  *
- * Copyleft 2018 Mark Jeronimus. All Rights Reversed.
+ * Copyleft 2019 Mark Jeronimus. All Rights Reversed.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
